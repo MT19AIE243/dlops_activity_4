4c4e300982b0750c6597c2ee79acf6bf86132a6d
Harshitha_Nagella <thisisharshithanagella@gmail.com>
2024-04-04 18:31:27 +0530

    updating the changes

--- activity_4.py
+++ activity_4.py
@@ -12,19 +12,13 @@ data = {
 }
 df = pd.DataFrame(data)
 
-# Encode categorical variables
 df = pd.get_dummies(df, columns=['location'])
 
-# Split dataset into training and testing sets
 X = df.drop('price', axis=1)
 y = df['price']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train a Random Forest Regressor
 model = RandomForestRegressor(random_state=42)
 model.fit(X_train, y_train)
-
-# Evaluate the model
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
-print(f"Mean Squared Error: {mse}")
